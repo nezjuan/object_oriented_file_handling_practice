83edcde7c71aa3574db97d85afed87cd4f5e7cc9
nezjuan <juanmartinezat9@gmail.com>
2026-04-29 20:19:02 +0800

    made the program1 object oriented, but the txt files being created are still outside its respective main and function code folder

--- even_odd_extractor_program/even_odd_extractor_functions.py
+++ even_odd_extractor_program/even_odd_extractor_functions.py
@@ -1,3 +1,23 @@
 import random
+import os
 
-def generate_numbers():
+base_dir = os.path.dirname(__file__)
+
+class NumberFileGenerator:
+    def number_generator(self):
+        with open(os.path.join(base_dir, "numbers.txt"), "w") as numbers:  
+            for line in range(20):
+                numbers.write(f"{random.randint(1,100)}\n")
+
+class EvenOddExtractor:
+    def even_extractor(self, numbers_list):
+        with open(os.path.join(base_dir, "even.txt"), "w") as even_numbers:
+            for nums in numbers_list:
+                if nums % 2 == 0:
+                    even_numbers.write(str(nums) + "\n")
+
+    def odd_extractor(self, numbers_list):
+        with open(os.path.join(base_dir, "odd.txt"), "w") as odd_numbers:
+            for nums in numbers_list:
+                if nums % 2 != 0:
+                    odd_numbers.write(str(nums) + "\n")
--- even_odd_extractor_program/even_odd_extractor_main.py
+++ even_odd_extractor_program/even_odd_extractor_main.py
@@ -1,21 +1,14 @@
-import random
+from even_odd_extractor_functions import NumberFileGenerator, EvenOddExtractor
 
-with (open("./numbers.txt","w")) as numbers:  
-    for line in range(20):
-        numbers.write(f"{random.randint(1,100)}\n")  
+num_txt_file = NumberFileGenerator()
+num_txt_file.number_generator()
 
 with (open("./numbers.txt","r")) as nums_infile:
     numbers_list = [int(line.strip()) for line in nums_infile]
 
-with (open("./even.txt","w")) as even_numbers:
-    for nums in numbers_list:
-        if nums % 2 == 0:
-            even_numbers.write(str(nums) + "\n")
-
-with (open("./odd.txt","w")) as odd_numbers:
-    for nums in numbers_list:
-        if nums % 2 != 0:
-            odd_numbers.write(str(nums) + "\n")
+even_odd_extractor = EvenOddExtractor()
+even_odd_extractor.even_extractor(numbers_list)
+even_odd_extractor.odd_extractor(numbers_list)
 
 print("=============================================================================\n"
       "[FINISHED OBJECTIVE]: even.txt and odd.txt created/extracted from numbers.txt\n"
