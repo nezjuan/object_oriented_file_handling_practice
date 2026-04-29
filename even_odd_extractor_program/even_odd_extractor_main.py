db93913643a019e910386fc1c1fb0269dc7c3984
nezjuan <juanmartinezat9@gmail.com>
2026-04-29 20:28:39 +0800

    FINALLY fixed the txt files directory issue

--- even_odd_extractor_program/even_odd_extractor_main.py
+++ even_odd_extractor_program/even_odd_extractor_main.py
@@ -1,15 +1,20 @@
 from even_odd_extractor_functions import NumberFileGenerator, EvenOddExtractor
+import os
+
+current_dir = os.path.dirname(__file__)
 
 num_txt_file = NumberFileGenerator()
 num_txt_file.number_generator()
 
-with (open("./numbers.txt","r")) as nums_infile:
+numbers_file_path = os.path.join(current_dir, "numbers.txt")
+
+with open(numbers_file_path, "r") as nums_infile:
     numbers_list = [int(line.strip()) for line in nums_infile]
 
 even_odd_extractor = EvenOddExtractor()
 even_odd_extractor.even_extractor(numbers_list)
 even_odd_extractor.odd_extractor(numbers_list)
 
-print("=============================================================================\n"
-      "[FINISHED OBJECTIVE]: even.txt and odd.txt created/extracted from numbers.txt\n"
-      "=============================================================================\n")
+print("----------------------------------------------------------\n"
+      " [FINISHED OBJECTIVE]: even.txt and odd.txt created/extracted from numbers.txt\n"
+      "----------------------------------------------------------\n")
