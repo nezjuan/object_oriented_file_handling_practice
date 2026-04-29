8b915ccd07b70b6a25b6b09c0c9075b0b8e01a30
nezjuan <juanmartinezat9@gmail.com>
2026-04-29 17:05:39 +0800

    finally fixed the squared and double files, now currently working on the triples and cubed for program4

--- squares_and_cubes_of_integers/squares_and_cubes.py
+++ squares_and_cubes_of_integers/squares_and_cubes.py
@@ -4,7 +4,10 @@ with (open("./integers.txt","w")) as numbers:
     for line in range(20):
         numbers.write(f"{random.randint(1,100)}\n")
 
+with (open("./numbers.txt","r")) as nums_infile:
+    numbers_list = [int(line.strip()) for line in nums_infile]
+
 with (open("./double.txt","w")) as squares:
-    for nums in squares:
+    for nums in numbers_list:
         squared = nums ** 2
-        squared.write(str(nums) + "\n")
+        squares.write(f"Base:{nums} = Squared: {squared}\n")
