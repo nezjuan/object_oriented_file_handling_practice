be0117b6e5168a9993685665f98e8f644824c764
nezjuan <juanmartinezat9@gmail.com>
2026-04-29 15:20:44 +0800

    finished the top student lister program, only needs tidying up like the previous program

--- students_gwa_lister/students_GWA_lister.py
+++ students_gwa_lister/students_GWA_lister.py
@@ -9,4 +9,17 @@ with (open("./students_GWA.txt","w")) as studs_GWA:
         "Caitlyn Kirraman", "Jayce Talis", "Sarah Fortune","Tobias Foxtrot",
         "Mel Medarda", "Renata Glasc", "Fiora Laurent","Ambessa Medarda"]
     for line in range(20):
-        studs_GWA.write(f"{random.choice(studs_name)}, GWA: {random.choice(gwa_scores)}\n")
+        studs_GWA.write(f"{random.choice(studs_name)}- GWA: {random.choice(gwa_scores)}\n")
+
+top_gwa=float("inf")
+top_stud=None
+with (open("./students_GWA.txt",'r')) as name_gwa:
+    for line in name_gwa:
+        name, gwa = line.strip().split('-')
+        gwa = float(gwa.replace("GWA:","").strip())
+        if gwa < top_gwa:
+            top_gwa = gwa
+            top_stud = name
+
+with (open("./honored_student.txt","w")) as honored_one:
+    honored_one.write(f"The Honored One: {top_stud}\nGeneral Weighted Average: {top_gwa}")
